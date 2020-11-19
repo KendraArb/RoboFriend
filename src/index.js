576d4dd8f7f4b134fc2c25aa9d76b5b543455fed
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-19 13:55:39 -0700

    Added service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { rootReducers } from './reducers';
 import { createLogger } from 'redux-logger';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import 'tachyons';
 import './assets/css/index.css';
@@ -28,3 +29,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
