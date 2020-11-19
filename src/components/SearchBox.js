bab9bac505f39b9ae0452adc7e196f97dddefa7d
Kendra Bryant <kendraa@utahcounty.gov>
2020-11-19 14:05:33 -0700

    Added input label

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,10 @@ import React from 'react';
 const SearchBox = ({ search }) => {
 	return (
 		<div className="pa2">
-			<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search Robots" onChange={search} />
+			<label htmlFor="searchbox" className="tracked-mega db mb2 f6 light-green georgia ttu">
+				Search Robots
+			</label>
+			<input id="searchbox" className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search Robots" onChange={search} />
 		</div>
 	);
 };
